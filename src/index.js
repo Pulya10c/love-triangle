42d8a8de424418bc5314f891022159a8212817cc
Pulya10c <sergienya.oleg@yandex.ru>
2018-09-16 13:48:16 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var lovecounter = 0;
+  var cut1=0;
+  var cut2=0;
+  for (var i = 0; i < (preferences.length); i++) {
+    var cut1 = preferences[i]-1;
+    var cut2 = preferences[cut1]-1;
+if ((preferences[cut2]-1)===i) {++lovecounter;};
+  };
+  
+  return Math.trunc(lovecounter/3);
+  
 };
